5ee8d8b4e5d7186d128b4dc442d173a76d783a16
pavo1hr <ivan96jp@gmail.com>
2019-04-26 02:07:04 +0200

    applied multiple changes

---
+++ ClickFinder/ClickFinder/Constants/AppVariables.swift
@@ -0,0 +1,14 @@
+//
+//  AppVariables.swift
+//  clickFinder
+//
+//  Created by Ivan Pavic on 23.04.19.
+//  Copyright © 2019 Ray Wenderlich. All rights reserved.
+//
+
+import Foundation
+
+class AppVariables{
+    static var pairingIsOn = false
+    static var mainPage = AppConstants.pairingPage
+}
